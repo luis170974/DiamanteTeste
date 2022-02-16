238f6799ea219593cf845a58ed4a371e126d3624
LUIS HENRIQUE FERREIRA KRAUS <luishenriquekraus@hotmail.com>
2022-02-16 14:37:41 -0300

    Add project files.

---
+++ DiamanteTeste.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DiamanteTeste.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de colunas");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            int quantidadeEspacos = (numero - 1) / 2;
+
+            int quantidadeX = 1;
+
+            int quantidadeLinhas = (numero - 1) / 2;
+
+            //parte de cima
+            for(int i = 0; i < quantidadeLinhas; i++)
+            {
+                for(int j = 0; j < quantidadeEspacos; j++)
+                {
+                    //printar espaços
+                    Console.Write(" ");
+                }
+
+                for(int k = 0; k < quantidadeX; k++)
+                {
+                    //printar X
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+                // diminuem os espaços e aumentam os X
+                quantidadeEspacos--;
+                quantidadeX += 2;
+
+            }
+
+            for (int i = 0; i <= quantidadeLinhas; i++)
+            {
+                for (int j = 0; j < quantidadeEspacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < quantidadeX; k++)
+                {
+                    Console.Write("x");
+                }
+                //aumenta os espaços e diminuem os X
+                quantidadeEspacos++;
+                quantidadeX -= 2;
+                Console.WriteLine();
+            }
+            //espera a interação com o usuario :)
+            Console.ReadLine();
+        }
+    }
+}
